977ea6a86224597b6497fefd8a3f45a3ef7221f3
ApplicateIT-Thang <kdt@applicateit.net>
2022-07-19 17:09:15 +0700

    deploy QA 3.0

--- Custom-HTTP-Probe/server.js
+++ Custom-HTTP-Probe/server.js
@@ -45,7 +45,7 @@ router.get("/",function(req,res){
             '/probe/readiness': 'Standard readiness probe',
             '/probe/liveness': 'Standard liveness probe',
             '/probe/custom': 'A strange custom probe...',
-            '/somethingnew':'QA 2.0....'
+            '/somethingnew':'QA 3.0....'
         }, 
         state: getState()
     }
