a9ffec8b9ffee53b997f647f08ad06873b9685f3
ApplicateIT-Thang <kdt@applicateit.net>
2022-07-20 09:45:43 +0700

    QA 6.0

--- Custom-HTTP-Probe/server.js
+++ Custom-HTTP-Probe/server.js
@@ -45,7 +45,7 @@ router.get("/",function(req,res){
             '/probe/readiness': 'Standard readiness probe',
             '/probe/liveness': 'Standard liveness probe',
             '/probe/custom': 'A strange custom probe...',
-            '/somethingnew':'QA 5.0....'
+            '/somethingnew':'QA 6.0....'
         }, 
         state: getState()
     }
